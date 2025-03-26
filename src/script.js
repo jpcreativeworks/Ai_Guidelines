2101780a0ddb30f50f5cc674c912a1aba1ca3988
Jenn <jp.graphics.011@gmail.com>
2025-03-26 13:12:26 -0700

    created carousel

--- src/script.js
+++ src/script.js
@@ -23,3 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+// image carousel
+const images = [
+  "img/Firefly_surrealist_giant_hourglass.jpg",
+  "img/Firefly_impressionist-style_oil_painting.jpg",
+  "img/Firefly_watercolor_majestic_coyote.jpg",
+  "img/Firefly_claymation-style_underwater_scene.jpg",
+  "img/Firefly_pixel_bustling_night_market.jpg",
+  "img/Firefly_ethereal_floating_island.jpg",
+  "img/Firefly_impressionist_flowers_with_burger.jpg",
+  "img/Firefly_historical_Albert_Einstein_teaching.jpg",
+  "img/Firefly_minimalist_mountain_landscape.jpg"
+];
+
+let currentImage = 0;
+const imageElement = document.querySelector('.carousel-image');
+
+document.querySelector('.prev').addEventListener('click', () => {
+  currentImage = (currentImage - 1 + images.length) % images.length;
+  imageElement.src = images[currentImage];
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  currentImage = (currentImage + 1) % images.length;
+  imageElement.src = images[currentImage];
+});
